ec1d14287b58d9cbf4d41a4c2246828b561d4905
jvnlee <lhjun1028@naver.com>
2025-02-27 20:08:18 +0900

    feat: DB 스키마 업데이트 자동화를 위해 alembic 의존성 추가

---
+++ app/model/__init__.py
@@ -0,0 +1,4 @@
+from app.model.user import User
+from app.model.reservation import Reservation
+from app.model.test_schedule import TestSchedule
+from app.model.reservation_test_schedule import reservation_test_schedule
