54ef355aa3a207fd2b8536855d60cbea1fcef0d6
jvnlee <lhjun1028@naver.com>
2025-03-03 04:49:32 +0900

    feat: reservations 테이블에서 is_confirmed 컬럼을 제거하고 status 컬럼 추가
    예약 확정 여부 외에도 구체적인 처리 상태를 나타내기 위함

--- app/model/reservation.py
+++ app/model/reservation.py
@@ -1,7 +1,15 @@
-from sqlalchemy import Column, Integer, ForeignKey, Boolean
+import enum
+from sqlalchemy import Column, Integer, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 from app.db.base import Base
 from app.model.reservation_test_schedule import reservation_test_schedule
+
+
+class ReservationStatus(str, enum.Enum):
+    REQUESTED = 'requested'
+    CONFIRMED = 'confirmed'
+    REJECTED = 'rejected'
+    CANCELLED = 'cancelled'
 
 
 class Reservation(Base):
@@ -11,7 +19,7 @@ class Reservation(Base):
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     test_schedule_id = Column(Integer, ForeignKey('test_schedules.id'), nullable=False)
     participant_num = Column(Integer, nullable=False)  # 시험 응시 인원
-    is_confirmed = Column(Boolean, nullable=False, default=False)  # 예약 확정 여부
+    status = Column(Enum(ReservationStatus, native_enum=False), nullable=False, default=ReservationStatus.REQUESTED.value)  # 예약 확정 여부
 
     user = relationship("User", back_populates='reservations')
     test_schedules = relationship(
