8d9ac2a2714cd5f1798adf05be5a7f75693c9eac
jvnlee <lhjun1028@naver.com>
2025-03-03 23:54:30 +0900

    feat: 쿼리 로그 옵션 추가

--- app/db/session.py
+++ app/db/session.py
@@ -10,5 +10,5 @@ DB_URL = (
     f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 )
 
-engine = create_engine(DB_URL, pool_pre_ping=True)
+engine = create_engine(DB_URL, pool_pre_ping=True, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
