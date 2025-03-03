49165aee7db3fe83504d94d805171248e0fd4bb7
jvnlee <lhjun1028@naver.com>
2025-03-03 21:30:45 +0900

    feat: TestScheduleItem에도 직렬화 관련 설정 추가

--- app/schema/test_schedule.py
+++ app/schema/test_schedule.py
@@ -5,8 +5,12 @@ from pydantic import BaseModel, Field
 
 
 class TestScheduleItem(BaseModel):
-    date_time: datetime = Field(..., examples=["2025-03-01T14:00:00"])
-    remaining_capacity: int = Field(..., examples=[50000])
+    date_time: datetime = Field(..., alias="dateTime", examples=["2025-03-01T14:00:00"])
+    remaining_capacity: int = Field(..., alias="remainingCapacity", examples=[50000])
+
+    class Config:
+        populate_by_name = True
+        by_alias = True
 
 
 class ReadAvailableTestSchedulesResponse(BaseModel):
