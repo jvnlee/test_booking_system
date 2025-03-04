f685608523fea1bc9c2c690a6506ece819d4ede7
jvnlee <lhjun1028@naver.com>
2025-03-04 09:02:22 +0900

    feat: 시험 일정 조회 결과가 없는 케이스 예외 처리

--- app/service/test_schedule.py
+++ app/service/test_schedule.py
@@ -1,11 +1,18 @@
 from datetime import datetime, date
 from sqlalchemy import Date, cast
 from sqlalchemy.orm import Session
+
+from app.exception.test_schedule_not_found_exception import TestScheduleNotFoundException
 from app.model import TestSchedule
 
 
 def read_available_test_schedules(db: Session, desired_date: date):
-    return db.query(TestSchedule).filter(
+    test_schedules = db.query(TestSchedule).filter(
         TestSchedule.remaining_capacity > 0,
         cast(TestSchedule.date_time, Date) == desired_date
     ).all()
+
+    if not test_schedules:
+        raise TestScheduleNotFoundException()
+
+    return test_schedules
